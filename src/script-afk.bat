2000aca375b1427f4b0f291376215b0a2d8d4acd
ronieremarques <ronieremarques55@gmail.com>
2024-11-03 18:25:49 -0300

    send

--- src/script-afk.bat
+++ src/script-afk.bat
@@ -2,7 +2,16 @@
 :loop
 rem Fazendo algo irrelevante
 echo "Processando..."
+powershell -command "$x = 100; $y = 100; [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($x, $y)"
 powershell -command "$wsh = New-Object -ComObject WScript.Shell; $wsh.SendKeys('{SCROLLLOCK}')"
-rem Esperando
-timeout /t 30 /nobreak
+powershell -command "$wsh = New-Object -ComObject WScript.Shell; $wsh.SendKeys('{F5}')"
+powershell -command "$wsh = New-Object -ComObject WScript.Shell; $wsh.SendKeys('{SHIFT}')"
+
+powershell -command "$wsh = New-Object -ComObject WScript.Shell; $wsh.SendKeys('^{t}')"
+timeout /t 1 /nobreak
+powershell -command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.SendKeys]::SendWait('github.com/ronieremarquesjs')"
+powershell -command "$wsh = New-Object -ComObject WScript.Shell; $wsh.SendKeys('{ENTER}')"
+timeout /t 600 /nobreak  // Ajustado para 10 minutos
+powershell -command "$wsh = New-Object -ComObject WScript.Shell; $wsh.SendKeys('^{w}')"
+timeout /t 600 /nobreak  // Espera mais 10 minutos antes de reiniciar o loop
 goto loop
